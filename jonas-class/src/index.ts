5ee1bfbe556f56fb10c827c4904527e90f7cd33d
ENGRSMYLE <101359280+ENGRSMYLE@users.noreply.github.com>
2025-01-12 21:26:17 +0100

    Changes

--- jonas-class/src/index.ts
+++ jonas-class/src/index.ts
@@ -1,31 +1,57 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import tourRouter from "./router/tourRouter";
 import userRouter from "./router/userRouter";
 import morgan from "morgan";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config({ path: "./config.evn" });
 
-dotenv.config({path: ' ./config.env'})
 
-console.log(process.env)
+
+mongoose
+  .connect(
+    "mongodb+srv://smyle:WjwkG5L0TAUjSFPQ@cluster.jfltm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster",
+    {}
+  )
+  .then((con) => {
+    console.log("Database Connected");
+  });
+
+// Database sCHEMA
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true],
+  },
+  rating: Number,
+  price: Number,
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The forest hiJSS",
+  rating: 4.9,
+  price: 50,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("Error");
+  });
 
 const app: Application = express();
 const PORT = 3000;
-
-
-
-const en = app.get('env') 
-
-
 
 //## Include middleware
 app.use(express.json());
 app.use("/api/v1/tours/", tourRouter);
 app.use("/api/v1/user", userRouter);
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  console.log("Hello From the middleware");
-  next();
-});
 
 // ### Server port
 app.listen(PORT, () => {
